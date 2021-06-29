89230148478af5439cff977f982477c3b77cdce1
sangdo913 <sangdo913@gmail.com>
2021-06-29 21:52:24 +0900

    sangdo 모의고사 solved

---
+++ SEASON_1/모의고사/sangdo_모의고사.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun solution(answers: IntArray): IntArray {
+        var num1 = intArrayOf(1,2,3,4,5)
+        var num2 = intArrayOf(2,1,2,3,2,4,2,5)
+        var num3 = intArrayOf(3,3,1,1,2,2,4,4,5,5)
+        var ok1 = 0
+        var ok2 = 0
+        var ok3 = 0
+        for(i in answers.indices) {
+            ok1 += if(num1[(i%num1.size)] == answers[i]) 1 else 0
+            ok2 += if(num2[i%num2.size] == answers[i]) 1 else 0
+            ok3 += if(num3[i%num3.size] == answers[i]) 1 else 0
+        }
+        var max = Math.max(ok1,ok2)
+        max = Math.max(max, ok3)
+        var num = 0
+        num += if(ok1 == max) 1 else 0
+        num += if(ok2 == max) 1 else 0
+        num += if(ok3 == max) 1 else 0
+
+        var res = IntArray(num)
+        var l = 0
+        if(max == ok1) res[l++] = 1
+        if(max == ok2) res[l++] = 2
+        if(max == ok3) res[l++] = 3
+
+        return res
+    }
+}
