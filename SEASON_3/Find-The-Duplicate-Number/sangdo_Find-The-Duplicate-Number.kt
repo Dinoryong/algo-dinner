aecde68632eb3a3c1a81805c8a04c65a6a547fde
Jiwoong Hwang <hzw94@naver.com>
2021-07-15 10:27:58 +0900

    Update sangdo_Find-The-Duplicate-Number.kt

--- SEASON_3/Find-The-Duplicate-Number/sangdo_Find-The-Duplicate-Number.kt
+++ SEASON_3/Find-The-Duplicate-Number/sangdo_Find-The-Duplicate-Number.kt
@@ -5,6 +5,7 @@ class Solution {
             while(dest != nums[i]) {
                 var to = nums[i]-1
                 if(nums[to] == nums[i]) return nums[i]
+                // 이부분 고쳐주세요우 ㅎㅎ - By Wolfram HWang
                 var t = nums[to]
                 nums[to] = nums[i]
                 nums[i] = t
